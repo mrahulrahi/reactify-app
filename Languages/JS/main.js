38998d1579bdc0feb0320c91a3e6d14dab27b785
mrahulrahi <mrahulrahi@gmail.com>
2025-02-08 23:33:33 +0530

    languages dir added

---
+++ Languages/JS/main.js
@@ -0,0 +1,17 @@
+function fullName(first, last) {
+    console.log(first + " " + last)
+}
+
+fullName('Rahul', 'Maurya')
+
+let num = 5;
+var age;
+let name;
+
+let message = "Hello World";
+let _message = "Hello World";
+let $message = "Hello World";
+num =10;
+
+console.log(num);
+console.log(age);
