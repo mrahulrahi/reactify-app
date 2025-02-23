e9e80fe78af305aa9eee25299b222bd54bc30aa6
mrahulrahi <mrahulrahi@gmail.com>
2025-02-23 23:16:23 +0530

    re init repo project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="min-h-screen bg-gradient-to-r from-red-400 to-amber-300 flex flex-col items-center justify-center">
-      <Header />
-      <main className="flex flex-col items-center">
-        <section className="mb-8">
-          <h2 className="text-3xl font-semibold text-white">Our Features</h2>
-          <ul className="mt-4 space-y-2">
-            <li className="text-white">Feature 1: Description of feature 1</li>
-            <li className="text-white">Feature 2: Description of feature 2</li>
-            <li className="text-white">Feature 3: Description of feature 3</li>
-          </ul>
-        </section>
-        <section className="mb-8">
-          <h2 className="text-3xl font-semibold text-white">Our Services</h2>
-          <ul className="mt-4 space-y-2">
-            <li className="text-white">Service 1: Description of service 1</li>
-            <li className="text-white">Service 2: Description of service 2</li>
-            <li className="text-white">Service 3: Description of service 3</li>
-          </ul>
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
