688856dbdcf3cea88a5b73930158ef9ffc3de957
mrahulrahi <mrahulrahi@gmail.com>
2025-03-18 23:52:29 +0530

    commit 18 mar

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -14,9 +14,9 @@ const ListGroup = ({ items, heading, onSelectItem }) => {
 
     return (
         <div className='w-1/2'>
-            <div className='fs-4 fw-bold mb-2'>{heading}</div>
-            <ul className="list-group">
-                {items.map((item) => <li key={item.id} className={selectedIndex === item.id ? 'list-group-item active' : 'list-group-item'} onClick={() => {
+            <div className='text-2xl font-bold mb-2'>{heading}</div>
+            <ul className="list-group flex flex-col rounded-md">
+                {items.map((item) => <li key={item.id} className={`list-group-item relative block py-2 px-4 ${selectedIndex === item.id ? ' active' : ''}`} onClick={() => {
                     setSelectedIndex(item.id);
                     onSelectItem(item);
                 }}>{item.name}</li>)}
